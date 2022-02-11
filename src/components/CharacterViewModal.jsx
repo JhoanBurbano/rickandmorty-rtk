1beb6c7c4f5b8946e30cb80a7b96f9389e3fc5ac
JhoanBurbano <jhoanburbano@unicauca.edu.co>
2022-02-11 00:39:40 -0500

    modal

--- src/components/CharacterViewModal.jsx
+++ src/components/CharacterViewModal.jsx
@@ -8,8 +8,8 @@ const CharacterViewModal = () => {
     const {view} = useSelector(state=>state.character)
     const dispatch = useDispatch()
     return (
-        <div style={{width: '100vw', height: '100vh', background: '#0004', top: 0, left:0}} className={`${!view.id?"d-none":"d-flex"} flex-column position-absolute top-0 left-0`}>
-            <img src={view.image} alt="" style={{width: '300px'}}/>
+        <div style={{width: '100vw', height: '100vh', background: '#0007', top: 0, left:0}} className={`${!view.id?"d-none":"d-flex"} flex-column justify-content-evenly align-items-center position-absolute top-0 left-0 animate__animated animate__fadeIn`}>
+            <img src={view.image} alt="" style={{width: '20vw'}}/>
             <Button onClick={()=>{dispatch(viewCharacter({}))}} >Clear</Button>
         </div>
     )
